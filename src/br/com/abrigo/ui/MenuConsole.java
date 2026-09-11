9b166de931afffca15af58be19c56867b33fa6f7
luiz <luizsantost4@gmail.com>
2026-09-11 10:24:58 -0300

    implementa menu inicial

---
+++ src/br/com/abrigo/ui/MenuConsole.java
@@ -0,0 +1,54 @@
+package br.com.abrigo.ui;
+
+import java.util.Scanner;
+
+public class MenuConsole {
+    Scanner sc = new Scanner(System.in);
+
+    public MenuConsole() {}
+
+    public void carregarMenu () {
+        int opcao = 0;
+        do{
+            exibirOpcoes();
+            String entrada = sc.nextLine();
+
+            try {
+                opcao = Integer.parseInt(entrada);
+                processarOpcao(opcao);
+            }catch(NumberFormatException e) {
+                System.out.println("\nOpção inválida. Digite apenas números inteiros.\n");
+                opcao = 0;
+            }
+        }while(opcao!=6);
+    }
+
+    private void exibirOpcoes() {
+        System.out.println("----- MENU PRINCIPAL -----");
+        System.out.println("1 - Cadastrar um novo pet");
+        System.out.println("2 - Alterar os dados do pet cadastrado");
+        System.out.println("3 - Deletar um pet cadastrado");
+        System.out.println("4 - Listar todos os pets cadastrados");
+        System.out.println("5 - Listar pets por algum critério (idade, nome, raça)");
+        System.out.println("6 - Sair");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    private void processarOpcao(int opcao) {
+        switch (opcao) {
+            case 1 -> System.out.println("\nCadastrar novo pet");
+            case 2 -> System.out.println("\nAlterar os dados do pet cadastrado");
+            case 3 -> System.out.println("\nDeletar os pets cadastrados");
+            case 4 -> System.out.println("\nListar todos os pets cadastrados");
+            case 5 -> System.out.println("\nListar pet por algum critério");
+            case 6 -> System.out.println("\nFinalizando programa...");
+            default -> {
+                if(opcao <= 0){
+                    System.out.println("\nOpção inválida. Digite apenas números inteiros positivos.\n");
+                }else if(opcao > 6){
+                    System.out.println("\nOpção inválida. Digite apenas números inteiros entre 1 e 6.\n");
+                }
+            }
+        }
+    }
+}
